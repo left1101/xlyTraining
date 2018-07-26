79ad5e87cba45bba74345dff7718047f077072b4
left1101 <xiru.xie@xiaoniangao.cn>
2018-07-26 08:51:42 +0800

    s

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import { Provider } from 'react-redux'
 import index from './reducers/index'
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 const logger = createLogger();
 const store = createStore(index, applyMiddleware(logger))
