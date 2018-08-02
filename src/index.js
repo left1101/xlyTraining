32165099382e563b1db1a94209a2bf497d814e51
left1101 <xiru.xie@xiaoniangao.cn>
2018-08-02 11:32:46 +0800

    s

--- src/index.js
+++ src/index.js
@@ -3,17 +3,37 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import { Provider } from 'react-redux'
+import { Router, Route, IndexRedirect, browserHistory, IndexLink } from 'react-router'
 import index from './reducers/index'
 import './index.css'
 import App from './App'
+import AddMessage from './containers/AddMessage'
+import WeChatMessage from './containers/WeChatMessage'
 import registerServiceWorker from './registerServiceWorker'
 
 const logger = createLogger();
-const store = createStore(index, applyMiddleware(logger))
+const store = createStore(index, applyMiddleware(logger));
+
+// const routes = <Route path="/" component={App}>
+//   <IndexRedirect to="/weChat" />
+//   <Route path="weChat" component={WeChatMessage}/>
+//   <Route path="addMessage" component={AddMessage}/>
+// </Route>
+
+const routes = [{
+  path: '/',
+  component: App,
+  indexRoute: { component: WeChatMessage },
+  childRoutes: [
+    { path: 'weChat', component: WeChatMessage },
+    { path: 'addMessage', component: AddMessage }
+  ]
+}]
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router routes={routes} history={browserHistory}/>
+    {/*<WeChatMessage />*/}
   </Provider>,
   document.getElementById('root')
 );
